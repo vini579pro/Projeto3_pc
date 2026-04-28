6d7c31192df3e3ae7fa3ccac20a1a991450b1283
vini579pro <viniciusprates57@gmail.com>
2026-04-27 22:17:17 -0300

    Add files via upload

---
+++ filabacklog.py
@@ -0,0 +1,31 @@
+class filabacklog:
+    def __init__(self):
+        self.dados = []
+    
+    def enqueue(self, jogo):
+        self.dados.append(jogo)
+    
+    def enqueue(self):
+        if self.is_empty():
+            return None
+        return self.dados.pop(0)
+
+    def is_empty(self):
+        return len(self.dados) == 0
+    
+    def mostrar(self):
+        if self.is_empty():
+            print("backlog vazio")
+            return
+        for i,jogo in enumerate(self.dados, start = 1):
+            print(f"ID: {i}")
+            jogo.exibir()
+    
+    def tamanho(self):
+        return len(self.dados)
+    
+    def contem(self, id):
+        for jogo in self.dados:
+            if jogo.id_jogo == id:
+                return True
+            return False
